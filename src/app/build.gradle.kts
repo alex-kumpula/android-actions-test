75ee0e8f8e8e98387b763ce62a66a511e2b353e4
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-22 23:31:06 -0600

    Test 5

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -2,42 +2,29 @@ plugins {
     alias(libs.plugins.android.application)
 }
 
-// Javadoc task for Android sources (Kotlin DSL)
 import org.gradle.external.javadoc.JavadocMemberLevel
-
-tasks.register<Javadoc>("generateJavadoc") {
-    group = "documentation"
-    description = "Generates Javadoc for main sources."
-    source = fileTree("src/main/java")
-    classpath += files(android.bootClasspath)
-    options.encoding = "UTF-8"
-    options.memberLevel = JavadocMemberLevel.PUBLIC
-}
-
 import com.android.build.gradle.api.ApplicationVariant
-import org.gradle.external.javadoc.JavadocMemberLevel
 
+// Generate Javadoc for all variants (Kotlin DSL)
 android.applicationVariants.all {
     val variant = this
     val capitalized = variant.name.replaceFirstChar { it.uppercaseChar() }
     tasks.register<Javadoc>("generate${capitalized}Javadoc") {
         group = "documentation"
         description = "Generate ${variant.name} Javadoc"
-        // Use Java sources only
         source = variant.javaCompileProvider.get().source
         destinationDir = file("$rootDir/doc/javadoc/")
-        failOnError = false
         exclude("**/BuildConfig.java")
-
         doFirst {
             val androidJar = "${android.sdkDirectory}/platforms/${android.compileSdkVersion}/android.jar"
             classpath = files(variant.javaCompileProvider.get().classpath) + files(androidJar)
-            (options as org.gradle.external.javadoc.StandardJavadocDocletOptions).addStringOption("-show-members", "package")
+            (options as org.gradle.external.javadoc.StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
             options.encoding = "UTF-8"
             options.memberLevel = JavadocMemberLevel.PUBLIC
         }
     }
 }
+
 
 android {
     namespace = "com.example.demoapp"
