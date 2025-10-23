dfc899bf48064fa100d2a14314780885fb911a69
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-23 03:25:24 -0600

    testaGIANGON

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -27,8 +27,9 @@ android.applicationVariants.all {
             opts.memberLevel = JavadocMemberLevel.PUBLIC
             // opts.addStringOption("subpackages", "com.example.demoapp")
             opts.addStringOption("exclude", "android.*:androidx.*:kotlin.*")
-            // opts.addStringOption("-limit-modules", "com.example.demoapp")
+            opts.addStringOption("-limit-modules", "com.example.demoapp")
             opts.addStringOption("-stacktrace")
+            //opts.addStringOption("-debug")
         }
     }
 }
