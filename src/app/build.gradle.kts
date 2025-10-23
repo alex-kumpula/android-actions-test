880f2115aff492beaf6f0c6c317c4876e3e9348d
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-22 23:28:34 -0600

    Test

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -14,6 +14,31 @@ tasks.register<Javadoc>("generateJavadoc") {
     options.memberLevel = JavadocMemberLevel.PUBLIC
 }
 
+import com.android.build.gradle.api.ApplicationVariant
+import org.gradle.external.javadoc.JavadocMemberLevel
+
+android.applicationVariants.all {
+    val variant = this
+    val capitalized = variant.name.replaceFirstChar { it.uppercaseChar() }
+    tasks.register<Javadoc>("generate${capitalized}Javadoc") {
+        group = "documentation"
+        description = "Generate ${variant.name} Javadoc"
+        // Use Java sources only
+        source = variant.javaCompileProvider.get().source
+        destinationDir = file("$rootDir/doc/javadoc/")
+        failOnError = false
+        exclude("**/BuildConfig.java")
+
+        doFirst {
+            val androidJar = "${android.sdkDirectory}/platforms/${android.compileSdkVersion}/android.jar"
+            classpath = files(variant.javaCompileProvider.get().classpath) + files(androidJar)
+            (options as org.gradle.external.javadoc.StandardJavadocDocletOptions).addStringOption("-show-members", "package")
+            options.encoding = "UTF-8"
+            options.memberLevel = JavadocMemberLevel.PUBLIC
+        }
+    }
+}
+
 android {
     namespace = "com.example.demoapp"
     compileSdk {
